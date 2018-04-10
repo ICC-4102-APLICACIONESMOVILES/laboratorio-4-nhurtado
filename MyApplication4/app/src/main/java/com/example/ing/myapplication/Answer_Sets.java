b13a4455a362b7c06ad8ad0c28e7ed337cb4f4e6
Nhurtado <nhurtado@miuandes.cl>
2018-04-10 12:08:23 -0300

    Modelo de preguntas y respuestas.

--- MyApplication4/app/src/main/java/com/example/ing/myapplication/Answer_Sets.java
+++ MyApplication4/app/src/main/java/com/example/ing/myapplication/Answer_Sets.java
@@ -1,9 +1,10 @@
 package com.example.ing.myapplication;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
 import android.support.annotation.NonNull;
-import android.arch.persistence.room.ForeignKey;
+
 import static android.arch.persistence.room.ForeignKey.CASCADE;
 
 /**
@@ -15,24 +16,26 @@ import static android.arch.persistence.room.ForeignKey.CASCADE;
         childColumns = "formId",
         onDelete = CASCADE))
 
-public class Questions {
+public class Answer_Sets {
     @NonNull
     @PrimaryKey(autoGenerate = true)
-    private int questionId;
+    private int answerSetId;
     @NonNull
     private int formId;
-    private String questionText;
-    private String questionType;
+    @NonNull
+    private int userId;
+    @NonNull
+    private int setNumber;
 
-    public Questions() {
+    public Answer_Sets() {
     }
 
-    public int getQuestionId() { return questionId; }
-    public void setQuestionId (int questionId) { this.questionId = questionId; }
+    public int getAnswerSetId() { return answerSetId; }
+    public void setAnswerSetId (int answerSetId) { this.answerSetId = answerSetId; }
     public int getFormId() { return formId; }
     public void setFormId (int formId) { this.formId = formId; }
-    public String getQuestionText() { return questionText; }
-    public void setQuestionText (String questionText) { this.questionText = questionText; }
-    public String getQuestionType() { return questionType; }
-    public void setQuestionType (String questionType) { this.questionType = questionType; }
+    public int getUserId() { return userId; }
+    public void setUserId (int userId) { this.userId = userId; }
+    public int getSetNumber() { return setNumber; }
+    public void setSetNumber (int setNumber) { this.setNumber = setNumber; }
 }
